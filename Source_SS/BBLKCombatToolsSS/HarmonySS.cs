f1611794f680c01fcdc5cb71e9bb68feba567012
BBLKepling <90485361+BBLKepling@users.noreply.github.com>
2023-05-02 19:57:15 -0400

    LTS

--- Source_SS/BBLKCombatToolsSS/HarmonySS.cs
+++ Source_SS/BBLKCombatToolsSS/HarmonySS.cs
@@ -11,29 +11,25 @@ namespace BBLKCombatToolsSS
         [HarmonyPostfix]
         public static void SelfConsume(Verb_ShootOneUse __instance)
         {
-            if (!(__instance.caster is Pawn)) { return; }
+            if (!(__instance.caster is Pawn)) return;
             Pawn pawn = __instance.caster as Pawn;
-            if (!pawn.equipment.GetDirectlyHeldThings().NullOrEmpty()) { return; }
+            if (pawn.equipment is null || !pawn.equipment.GetDirectlyHeldThings().NullOrEmpty()) return;
             List<Thing> pawnInv = pawn.inventory?.innerContainer?.InnerListForReading;
-            if (pawnInv.NullOrEmpty()) { return; }
+            if (pawnInv.NullOrEmpty()) return;
             //pawnInv.Shuffle();
             //pawnInv.Reverse();
             foreach (Thing thing in pawnInv)
-            {
                 if (thing.def.IsRangedWeapon)
                 {
                     pawn.inventory.innerContainer.TryTransferToContainer(thing, pawn.equipment.GetDirectlyHeldThings(), 1, false);
                     return;
                 }
-            }
             foreach (Thing thing in pawnInv)
-            {
                 if (thing.def.IsWeapon)
                 {
                     pawn.inventory.innerContainer.TryTransferToContainer(thing, pawn.equipment.GetDirectlyHeldThings(), 1, false);
                     return;
                 }
-            }
         }
     }
 }
